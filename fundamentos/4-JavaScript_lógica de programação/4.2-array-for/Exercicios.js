c78f17bdd4594d53c29fbfd2062c277c8ef3a3c5
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2021-11-29 16:19:56 -0300

    adiciona exercicio array & for

---
+++ fundamentos/4-JavaScript_lógica de programação/4.2-array-for/Exercicios.js
@@ -0,0 +1,41 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+/* Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() */
+/* for(let number of numbers) {
+  console.log(number)
+}
+ */
+
+/* Para o segundo exercício, some todos os valores contidos no array e imprima o resultado; */
+/* let sun = 0
+for(let number of numbers) {
+  sun += number
+}
+console.log(sun) */
+
+//Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+/* let sun = 0
+for(let number of numbers) {
+  sun += number
+}
+let media = sun / numbers.length
+console.log(media)
+
+if(media > 20){
+  console.log('Numero maior que 20')
+}else {
+  console.log('Numero menor ou igual a 20')
+} */
+
+
+/* Utilizando for , descubra qual o maior valor contido no array e imprima-o;
+let big = 0
+for(let number of numbers) {
+  if (number > big) {
+    big = number
+  }
+}
+console.log(big) */
+
+
+
