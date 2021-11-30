ab1e0820df49930497b20a0753ca22bd7b83c9e6
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2021-11-30 15:57:58 -0300

    adiciona exercicios 1 e 2

---
+++ fundamentos/4-JavaScript_lógica de programação/4.3-logica-programacao/exercicio.js
@@ -0,0 +1,19 @@
+//Quadrado de asteriscos
+/*
+let line = ''
+let number = 10
+for(let i = 1; i <= number; i+=1) {
+  line += '*'
+}
+for(let j = 1; j <= number; j+=1){
+  console.log(line)
+}
+*/
+
+// Triangulo retangulo de asteriscos
+let line = ''
+let number = 10
+for(let i = 1; i <= number; i+=1) {
+  line += '*'
+  console.log(line)
+}
