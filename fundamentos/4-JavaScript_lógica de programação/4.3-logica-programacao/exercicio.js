0b8a46b219c7fcebbd5ff01e1250ad4cf9123b55
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2021-11-30 16:37:28 -0300

    exercicio ttriangulo invertido

--- fundamentos/4-JavaScript_lógica de programação/4.3-logica-programacao/exercicio.js
+++ fundamentos/4-JavaScript_lógica de programação/4.3-logica-programacao/exercicio.js
@@ -11,9 +11,24 @@ for(let j = 1; j <= number; j+=1){
 */
 
 // Triangulo retangulo de asteriscos
-let line = ''
+/* let line = ''
 let number = 10
 for(let i = 1; i <= number; i+=1) {
   line += '*'
+  console.log(line)
+} */
+
+let line = ''
+let number = 5
+for(let i = 1; i <= number; i+=1) {
+  line = ''
+  let space = number - i
+  
+  for(space; space > 0; space -= 1 ){
+    line += '/'
+  }
+  for(let j = line.length; j < number; j += 1){
+    line += '*'
+  }
   console.log(line)
 }
