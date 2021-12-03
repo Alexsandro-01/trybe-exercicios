0c2472e0e9685df0a34abd522831cfe3c34f2d6c
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2021-12-03 20:25:45 -0300

    mufasou

--- fundamentos/5-DOM-Events_WebStorage/5.1-Dom/script.js
+++ fundamentos/5-DOM-Events_WebStorage/5.1-Dom/script.js
@@ -1,13 +1,19 @@
+let head = document.querySelector('header')
+head.style.background = 'rgb(76,164,109)'
+
+let foot = document.querySelector('footer')
+foot.style.background = 'rgb(76,164,109)'
+
 let emergency = document.querySelector('.emergency-tasks')
 let emergency_h3 = document.querySelectorAll('.emergency-tasks h3')
 
 let noEmergency = document.querySelector('.no-emergency-tasks')
 let noEmergency_h3 = document.querySelectorAll('.no-emergency-tasks h3')
 
-emergency.style.background = '#333'
-emergency_h3[0].style.background = 'darkred'
-emergency_h3[1].style.background = 'darkred'
+emergency.style.background = 'rgb(76,164,109)'
+emergency_h3[0].style.background = 'orangered'
+emergency_h3[1].style.background = 'orangered'
 
-noEmergency.style.background = '#999'
+noEmergency.style.background = 'rgb(100,164,200)'
 noEmergency_h3[0].style.background = 'goldenrod'
 noEmergency_h3[1].style.background = 'goldenrod'
