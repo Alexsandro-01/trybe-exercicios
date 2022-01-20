81fbdc1abec874898492e977297f530968d1311f
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-01-20 20:51:12 -0300

    exercicio 7 concluido

---
+++ fundamentos/8-Higher-Order-Functions/8.3-FIlter-Map/exercise.js
@@ -0,0 +1,32 @@
+const books = require('./books');
+
+// Encontre o nome do livro escrito pela pessoa cujo nome registrado começa com três iniciais.
+
+const expectedResult = 'O Senhor dos Anéis';
+
+function authorWith3DotsOnName(books) {
+  // escreva seu código aqui
+  const arrayNames = [];
+  books.forEach(element => {
+    arrayNames.push(element.author.name);
+  });
+
+  const result = arrayNames.find( (nome) => {
+    let count = 0;
+    nome.split('').forEach( e => {
+      if (e === '.') {
+        count += 1;
+      }
+    });
+
+    if(count === 3) {
+      return nome;
+    }
+
+    count = 0
+  })
+
+  
+console.log(result);
+}
+authorWith3DotsOnName(books);
