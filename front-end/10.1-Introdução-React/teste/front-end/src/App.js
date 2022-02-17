991e83ff3acdf9234159325d9d05c003aa4aaa5e
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-02-17 18:54:45 -0300

    conteudo

--- front-end/10.1-Introdução-React/teste/front-end/src/App.js
+++ front-end/10.1-Introdução-React/teste/front-end/src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
+import img from './logo.svg';
 import './Header';
 import Header from './Header';
 import Content from './Content';
 import Footer from './Footer';
+import Image from './Image';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        {/* <div> */}
+        <Image source={img} alternativeText="Cute cat starting"/>
         <Header />     
+        {/* </div> */}
       </header>
       <main>
         <Content />
