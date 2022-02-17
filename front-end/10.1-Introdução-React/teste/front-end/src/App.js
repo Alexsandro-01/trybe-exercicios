d1f1019ddf7719a5f2e17ebe25ff2bc5d43603d2
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-02-16 22:11:48 -0300

    Exercicio do dia completo

---
+++ front-end/10.1-Introdução-React/teste/front-end/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import './Header';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Header />     
+      </header>
+      <main>
+        <Content />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
+}
+
+export default App;
