a187f5f47485efdd4a54a48457382c2bd43b5ccc
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-03-21 20:09:29 -0300

    pendencias

--- front-end/14.3-testanto-react-router/conteudo/test-app/src/App.test.js
+++ front-end/14.3-testanto-react-router/conteudo/test-app/src/App.test.js
@@ -1,8 +1,51 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import renderWithRouter from './renderWithRouter';
+import userEvent from '@testing-library/user-event';
+import App, { About } from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Teste a aplicação toda', () => {
+  test('Deve renderizar o component App', () => {
+    renderWithRouter(<App />);
+  
+    const homeTitle = screen.getByRole('heading', {
+      name: 'Você está na página Início',
+    });
+    expect(homeTitle).toBeInTheDocument();
+  });
+  
+  test('Deve renderizar o componente sobre', () => {
+    const { history } = renderWithRouter(<App />);
+  
+    const aboutLink = screen.getByRole('link', { name: 'Sobre'});
+    expect(aboutLink).toBeInTheDocument();
+    userEvent.click(aboutLink);
+  
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  
+    const aboutTitle = screen.getByRole('heading', {
+      name: 'Você está na página Sobre'
+    });
+    expect(aboutTitle).toBeInTheDocument();
+  })
+
+  test('Deve testar a renderização do Not Found para um caminho não existente', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/pagina/que-nao-existe/');
+
+    const notFoundTitle = screen.getByRole('heading', {
+      name: 'Página não encontrada'
+    });
+    expect(notFoundTitle).toBeInTheDocument();
+  })
+
+  test('Deve renderizar o component About (Apenas component', () => {
+    renderWithRouter(<About />);
+
+    const aboutTitle = screen.getByRole('heading', {
+      name: 'Você está na página Sobre'
+    });
+    expect(aboutTitle).toBeInTheDocument();
+  });
+})
