5894b36bdbf8704e0357dd8c8314a1041a8614fe
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-05-31 14:46:02 -0300

    conclusao do exercicio 21.1-funcoes mysql

--- back-end/21.1-funcoe-mais-usadas-SQL/EXERCICIOS.sql
+++ back-end/21.1-funcoe-mais-usadas-SQL/EXERCICIOS.sql
@@ -34,15 +34,100 @@
     employees;
     
 -- 6. Escreva uma query que exiba a quantidade de pessoas que trabalham como pessoas programadoras (IT_PROG).
+  SELECT
+    JOB_ID,
+    COUNT(*) as total
+  FROM
+    employees
+  GROUP BY
+    JOB_ID
+  HAVING
+    JOB_ID = 'IT_PROG';
+
 -- 7. Escreva uma query que exiba a quantidade de dinheiro necessária para efetuar o pagamento de cada profissão (JOB_ID).
+  SELECT
+    JOB_ID,
+    SUM(salary)
+  FROM
+    employees
+  GROUP BY
+    JOB_ID;
+
 -- 8. Utilizando a query anterior, faça as alterações para que seja exibido somente a quantidade de dinheiro necessária para cobrir a folha de pagamento das pessoas programadoras (IT_PROG).
+  SELECT
+    JOB_ID,
+    SUM(salary)
+  FROM
+    employees
+  GROUP BY
+    JOB_ID
+  HAVING JOB_ID = 'IT_PROG';
+
 -- 9. Escreva uma query que exiba em ordem decrescente a média salarial de todos os cargos, exceto das pessoas programadoras (IT_PROG).
+  SELECT
+    JOB_ID,
+    AVG(salary) as `media`
+  FROM
+    employees
+  GROUP BY
+    JOB_ID
+  HAVING
+    JOB_ID != 'IT_PROG'
+  ORDER BY 
+    `media` DESC;
+
 -- 10. 🚀 Escreva um query que exiba média salarial e o número de funcionários de todos os departamentos com mais de dez funcionários. Dica: agrupe pelo DEPARTMENT_ID.
+  SELECT
+    DEPARTMENT_ID,
+    COUNT(department_id) as `quantidade`,
+    AVG(salary) as `media`
+  FROM
+    employees
+  GROUP BY
+    DEPARTMENT_ID
+  HAVING
+    `quantidade` > 10;
+
 -- 11. 🚀 Escreva uma query que atualize a coluna PHONE_NUMBER, de modo que todos os telefones iniciados por 515 agora devem iniciar com 777.
+  UPDATE
+    employees
+  SET
+    PHONE_NUMBER = REPLACE(PHONE_NUMBER, '515', '777')
+  WHERE
+    PHONE_NUMBER LIKE '515%';
+
 -- 12. Escreva uma query que só exiba as informações dos funcionários cujo o primeiro nome tenha oito ou mais caracteres.
+  SELECT
+    *
+  FROM
+    employees
+  WHERE
+    LENGTH(first_name) >= 8;
+
 -- 13. Escreva uma query que exiba as seguintes informações de cada funcionário: id, primeiro nome e ano no qual foi contratado (exiba somente o ano).
+SELECT
+    EMPLOYEE_ID,
+    FIRST_NAME,
+    year(HIRE_DATE)
+  FROM
+    employees;
+
 -- 14. 🚀 Escreva uma query que exiba as seguintes informações de cada funcionário: id, primeiro nome e dia do mês no qual foi contratado (exiba somente o dia).
+  SELECT
+    EMPLOYEE_ID,
+    FIRST_NAME,
+    day(HIRE_DATE)
+  FROM
+    employees;
+
 -- 15. Escreva uma query que exiba as seguintes informações de cada funcionário: id, primeiro nome e mês no qual foi contratado (exiba somente o mês).
+SELECT
+    EMPLOYEE_ID,
+    FIRST_NAME,
+    month(HIRE_DATE)
+  FROM
+    employees;
+    
 -- 16. Escreva uma query que exiba os nomes dos funcionários em letra maiúscula.
 -- 17: Escreva uma query que exiba o sobrenome e a data de contratação de todos os funcionário contratados em julho de 1987.
 -- 18: 🚀 Escreva uma query que exiba as seguintes informações de cada funcionário: nome, sobrenome, tempo que trabalha na empresa (em dias).
