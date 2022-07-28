023809373d9624de0072f0ed2eabd0208aff9e4e
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-07-28 20:11:52 -0300

    finalização da api

--- back-end/26.3-Express-Typescript/conteudo/express-typescript/index.ts
+++ back-end/26.3-Express-Typescript/conteudo/express-typescript/index.ts
@@ -22,20 +22,39 @@ app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   const { name, message, details } = err as any;
   console.log(`name: ${name}`);
 
-  switch (name) {
-    case 'ValidationError':
-      res.status(400).json({ message: details[0].message });
-      break;
-    case 'NotFoundError':
-      res.status(404).json({ message });
-      break;
-    case 'ConflictError':
-      res.status(409).json({ message });
-      break;
-    default:
-      console.error(err);
-      res.sendStatus(500);
+  const errors: { [erroName: string]: number } = {
+    ValidationError: 400,
+    NotFoundError: 404,
+    ConflictError: 409
   }
+
+  const status = errors[name];
+
+  if (!status) {
+    return res.status(500).json({ message });
+  }
+
+  if (status === 400) {
+    return res.status(500).json({ message: details[0].message });
+  }
+
+  res.status(status).json({ message });
+
+
+  // switch (name) {
+  //   case 'ValidationError':
+  //     res.status(400).json({ message: details[0].message });
+  //     break;
+  //   case 'NotFoundError':
+  //     res.status(404).json({ message });
+  //     break;
+  //   case 'ConflictError':
+  //     res.status(409).json({ message });
+  //     break;
+  //   default:
+  //     console.error(err);
+  //     res.sendStatus(500);
+  // }
 
   next();
   
--- back-end/26.3-Express-Typescript/conteudo/express-typescript/services/books.service.ts
+++ back-end/26.3-Express-Typescript/conteudo/express-typescript/services/books.service.ts
@@ -3,6 +3,7 @@
 import connection from '../models/connection';
 import BookModel from '../models/book.model';
 import Book from '../interfaces/book.interface';
+import { NotFoundError } from 'restify-errors';
 
 class BookService {
   public model: BookModel;
@@ -20,6 +21,29 @@ class BookService {
     const book = await this.model.getById(id);
     return book;
   }
+
+  public create(book: Book): Promise<Book> {
+    return this.model.create(book);
+  }
+
+  public async update(id: number, book: Book): Promise<void> {
+    const bookFound = await this.model.getById(id);
+    if (!bookFound) {
+      throw new NotFoundError('NotFoundError');
+    }
+
+    return this.model.update(id, book);
+  }
+
+  public async remove(id: number): Promise<void> {
+    const bookFound = await this.model.getById(id);
+    if (!bookFound) {
+      throw new NotFoundError('NotFoundError');
+    }
+
+    this.model.remove(id);
+  }
+  
 }
 
 export default BookService;
