d31a8822984b9504d9c4b98ec15616f0ca1340bc
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2022-07-28 14:44:11 -0300

    26.1-Typescript

---
+++ back-end/26.1-Typescript/exercise-day/yearStation.ts
@@ -0,0 +1,25 @@
+enum months {
+  january = 1,
+  february,
+  march,
+  april,
+  may,
+  juny,
+  julhy,
+  august,
+  septemble,
+  octuble,
+  novemble,
+  decemble
+}
+
+enum stations {
+  autumn,
+  winter,
+  sprin,
+  summer,
+}
+
+console.log(stations[0]);
+
+
